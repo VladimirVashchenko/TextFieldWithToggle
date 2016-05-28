d2195b3a54e561728cc8e758467b730ea15b7d44
VladimirVashchenko <vashchenko.vs.88@gmail.com>
2016-05-28 03:43:22 +0300

    Experiments with SVG path

--- src/customComponent/CustomTextField.java
+++ src/customComponent/CustomTextField.java
@@ -1,12 +1,7 @@
 package customComponent;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.scene.Cursor;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Skin;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
 
 public class CustomTextField extends PasswordField {
     private CustomSkin customSkin;
